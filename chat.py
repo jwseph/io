8106d660f93814f3aed1bfd680110b2615c659d9
kamiakhs <admin@kamiak.org>
2022-09-03 09:24:33 -0700

    update

--- chat.py
+++ chat.py
@@ -46,7 +46,7 @@ async def on_startup():
 async def refresh_token():
   async with aiohttp.ClientSession() as s:
     async with s.get(pc.endpoint+'/userinfo', params={'getauth': 1, 'username': os.environ['STORAGE_EMAIL'], 'password': os.environ['STORAGE_PASSWORD']}) as r:
-      # pc.auth = (await r.json())['auth']
+      pc.auth = (await r.json())['auth']
       print('REFRESH TOKEN')
       print(await r.json())
 
