a80aa785ee791424f425a85ecffcd3350eada699
jwseph <kodomo.oishii@gmail.com>
2023-06-10 22:45:45 -0700

    Fixed bugs, also only one event at a time now

--- giftbox.py
+++ giftbox.py
@@ -3,11 +3,26 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 import uuid
 import random
+import time
 
 from giftbox_fb import ref
 
 def generate_id() -> str:
   return uuid.uuid4().hex
+
+def get_all_events() -> list:
+  events = ref.child('events').get() or {}
+  for event in events.values():
+    if 'gifts' not in event:
+      event['gifts'] = {}
+  return events
+
+def get_event_id() -> str:
+  now = time.time_ns()//10**6  # UTC, might not correspond to PST times
+  for event_id, event in get_all_events().items():
+    if event['start_ms'] <= now < event['end_ms']:
+      return event_id
+  raise RuntimeError('No event is active right now')
 
 app = FastAPI()
 app.add_middleware(
@@ -25,13 +40,9 @@ async def home():
   return 'giftbox'
 
 @app.get('/get_events')
-async def get_events(password: str) -> dict[dict]:
+async def get_events(password: str):
   assert password == PASSWORD
-  events = ref.child('events').get() or {}
-  for event in events.values():
-    if 'gifts' not in event:
-      event['gifts'] = {}
-  return events
+  return get_all_events()
 
 @app.post('/add_event')
 async def add_event(password: str, event_name: str, event_start_ms: int, event_end_ms: int):
@@ -44,35 +55,34 @@ async def add_event(password: str, event_name: str, event_start_ms: int, event_e
   })
 
 @app.post('/add_gift')
-async def add_gift(password: str, event_id: str, gift_name: str, gift_quantity: int, gift_points: int):
+async def add_gift(password: str, gift_name: str, gift_quantity: int, gift_points: int):
   assert password == PASSWORD
   gift_id = generate_id()
-  ref.child(f'events/{event_id}/gifts/{gift_id}').set({
+  ref.child(f'events/{get_event_id()}/gifts/{gift_id}').set({
     'name': gift_name,
     'quantity': gift_quantity,
     'points': gift_points,
   })
 
 @app.post('/update_gift')
-async def update_gift(password: str, event_id: str, gift_id: str, gift_name: str = None, gift_quantity: int = None, gift_points: int = None):
+async def update_gift(password: str, gift_id: str, gift_name: str = None, gift_quantity: int = None, gift_points: int = None):
   assert password == PASSWORD
   gift = {}
   if gift_name is not None: gift['name'] = gift_name
   if gift_quantity is not None: gift['quantity'] = gift_quantity
   if gift_points is not None: gift['points'] = gift_points
-  ref.child(f'events/{event_id}/gifts/{gift_id}').update(gift)
+  ref.child(f'events/{get_event_id()}/gifts/{gift_id}').update(gift)
 
 @app.get('/pick_gift')
-async def pick_gift(password: str, event_id: str) -> str:
+async def pick_gift(password: str):
   assert password == PASSWORD
-  gifts = ref.child(f'events/{event_id}/gifts').get()
+  gifts = ref.child(f'events/{get_event_id()}/gifts').get()
   n = sum(gift['points'] for gift in gifts.values() if gift['quantity'])
   i = int(random.random()*n)
-  for gift_id, gift in gifts.values():
+  for gift_id, gift in gifts.items():
     if not gift['quantity']: continue
+    if i >= 0: choice = gift_id
     i -= gift['points']
-    if i > 0:
-      choice = gift_id
   gifts[choice]['quantity'] -= 1
   return choice
 
